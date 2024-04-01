1b68521600f93daed5be98e2b39743c09f8edbe8
Sana <darkking7806@gmail.com>
2024-04-01 19:36:12 +0530

    initial backend dir setup

---
+++ api/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use("/api/auth", authRoute);
+
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
